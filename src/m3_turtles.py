3601d6a9da02e457a2d42f0e67fd9a60d45809ec
johnsom6 <johnsom6@rose-hulman.edu>
2020-06-07 11:44:25 -0400

    attempt 1

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Madi Johnson.
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -63,6 +63,10 @@ dave = rg.SimpleTurtle()
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
+dave.left(45)
+dave.backward(100)
+dave.right(45)
+dave.forward(50)
 
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
@@ -75,9 +79,11 @@ matt.left(90)
 matt.forward(50)
 
 
+
+
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +97,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +115,22 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+billy = rg.SimpleTurtle()    #constructs the SimpleTurtle
+billy.pen = rg.Pen('blue', '15')     #characteristics
+
+# billy movements
+
+billy.forward(100)
+billy.left(90)
+billy.forward(25)
+billy.right(90)
+billy.backward(100)
+billy.right(90)
+billy.forward(25)
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
